1dc2c2b604a3b339b66a577cd97eb780966044ef
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-07-18 22:33:35 +0630

    implement companion card
    implement companion card with  props

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 
 const Page = () => {
   return (
-    <div><h1 className='text-xl underline'>Welcome</h1>
-    <Button variant="outline">Let&apos;s get started</Button>
-    </div>
+    <main>
+      <h1 className='text-xl underline'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard 
+        id='123'
+        name = 'Lession Number One'
+        topic = 'Some Topic'
+        subject = 'science'
+        duration = {45}
+        color = '#E5D0FF'
+        />
+        <CompanionCard 
+        id='124'
+        name = 'Lession Number Two'
+        topic = 'Some Topic'
+        subject = 'science'
+        duration = {45}
+        color = '#FFDA6E'
+        />
+        <CompanionCard 
+        id='125'
+        name = 'Lession Number Three'
+        topic = 'Some Topic'
+        subject = 'Maths'
+        duration = {45}
+        color = '#BDE7FF'
+        />
+
+      </section>
+      <section className='home-section'>
+        <CompanionsList/>
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
