3bb63faa1d00e720c650f99d01dd21de61ce8cae
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-07-21 11:58:48 +0630

    implement: fetch companions and display on Companion Library
    with distinct colors

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,30 @@
+import CompanionCard from '@/components/CompanionCard';
+import { getAllCompanions } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
 import React from 'react'
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+  const companions = await getAllCompanions({ subject, topic })
+  console.log('Companions:', companions)
   return (
-    <div>CompanionsLibrary</div>
+    <main>
+      <section className='flex justify-between gap-4 max-sm:flex-col'>
+        <h1> Companion Library</h1>
+          <div className="flex gap-4"> Filters</div>
+      </section>
+      <section className='companions-grid'>
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id}
+           {...companion}
+          //  pass them all by destructuring it with ...
+          color={getSubjectColor(companion.subject)}
+           />
+        )) }
+      </section>
+    </main>
   )
 }
 
