ec188d44d25090f34a3b0e1402c5598e4b45463b
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-07-21 17:18:57 +0630

    implement search input and filters

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,4 +1,6 @@
 import CompanionCard from '@/components/CompanionCard';
+import SearchInput from '@/components/SearchInput';
+import SubjectFilter from '@/components/SubjectFilter';
 import { getAllCompanions } from '@/lib/actions/companion.actions';
 import { getSubjectColor } from '@/lib/utils';
 import React from 'react'
@@ -8,12 +10,14 @@ const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
   const subject = filters.subject ? filters.subject : '';
   const topic = filters.topic ? filters.topic : '';
   const companions = await getAllCompanions({ subject, topic })
-  console.log('Companions:', companions)
   return (
     <main>
       <section className='flex justify-between gap-4 max-sm:flex-col'>
         <h1> Companion Library</h1>
-          <div className="flex gap-4"> Filters</div>
+          <div className="flex gap-4"> 
+            <SearchInput/>
+            <SubjectFilter/>
+          </div>
       </section>
       <section className='companions-grid'>
         {companions.map((companion) => (
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -5,7 +5,6 @@ import { createSupabaseClient } from "../supabase";
 
 export const createCompanion = async (formData: CreateCompanion) => {
     const { userId: author } = await auth(); //rename the clerk_userId as author
-    console.log('author:', author);
     const supabase = createSupabaseClient();
     const { data, error } = await supabase
         .from('companions')
@@ -34,4 +33,14 @@ export const getAllCompanions = async ({ limit = 10, page = 1, subject, topic }:
     if (error) throw new Error(error.message);
 
     return companions;
+}
+
+export const getCompanion = async (id: string) => {
+    const supabase = createSupabaseClient();
+    const {data, error} = await supabase
+        .from('companions')
+        .select()
+        .eq('id', id);
+    if(error) return console.log(error);
+    return data[0];
 }
